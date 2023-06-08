74562ce1aa3b3ee6943db021cd58ad11fb675453
Aarti <aarti46858@gmail.com>
2023-06-08 16:11:52 +0530

    This is my second commit.

---
+++ app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,40 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+    public static String question[]={
+            "Android is -",
+            "For which of the following Android is mainly developed?",
+            "Which of the following virtual machine is used by the Android operating system?",
+            "Android is based on which of the following language?",
+            "APK stands for -",
+            "What does API stands for?",
+            "Which of the following converts Java byte code into Dalvik byte code?",
+            "What is an activity in android?",
+            "We require an AVD to create an emulator. What does AVD stands for?",
+            "Which of the following is contained in the src folder?"
+
+    };
+    public static String choices[][]={
+            {"an operating system","a web browser","a web server","None of the above"},
+            {"Servers","Desktops","Laptops","Mobile devices"},
+            {"JVM","Dalvik virtual machine","Simple virtual machine","None of the above"},
+            {"Java","C++","C","None of the above"},
+            {"Android Phone Kit","Android Page Kit","Android Package Kit","None of the above"},
+            {"Application Programming Interface","Android Programming Interface","Android Page Interface","Application Page Interface"},
+            {"Dalvik converter","Dex compiler","Mobile interpretive compiler(MIC)","None of the above"},
+            {"android class","android package","A single screen in an application with supporting java code","None of the above"},
+            {"Android Virtual device","Android Virtual display","Active Virtual display","Active Virtual device"},
+            {"XML","Java source code","Manifest","None of the above"}
+    };public static String correctAnswers[]={
+            "an operating system",
+            "Mobile devices",
+            "Dalvik virtual machine",
+            "Java",
+            "Android Package Kit",
+            "Application Programming Interface",
+            "Dex compiler",
+            "A single screen in an application with supporting java code",
+            "Android Virtual device",
+            "Java source code"
+    };
+}
